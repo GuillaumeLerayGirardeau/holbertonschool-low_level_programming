bddd85313ac9e9da1d8859d904f5b68d737a901b
GuillaumeLerayGirardeau <guiraydeau@gmail.com>
2025-06-17 14:14:24 +0000

    5-print_numbers.c program added, print all single digit numbers of base 10

---
+++ variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int number = 0;
+
+	for (; number <= 9; number++)
+	{
+		printf("%d", number);
+
+	}
+	putchar('\n');
+	return (0);
+}
