f53cc74ec99258665b3efea81eaed3a4d56bcdf0
GuillaumeLerayGirardeau <guiraydeau@gmail.com>
2025-06-19 13:00:26 +0000

    2-print_alphabet_x10.c function added, prints alphabet 10 times

---
+++ functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+  *print_alphabet_x10 - print the alphabet 10 times
+  *
+  */
+void print_alphabet_x10(void)
+{
+	int time = 1;
+
+	while (time <= 10)
+	{
+		print_alphabet();
+		time++;
+	}
+}
