aa493845defdb595305f3b0555c2fd352815b4ef
GuillaumeLerayGirardeau <guiraydeau@gmail.com>
2025-06-20 15:22:16 +0000

    8-24_hours.c function updated, wrong betty coding style

--- functions_nested_loops/8-24_hours.c
+++ functions_nested_loops/8-24_hours.c
@@ -6,28 +6,28 @@
   */
 void jack_bauer(void)
 {
-	int grande_heure = 0;
-	int petite_heure = 0;
-	int grande_minute = 0;
-	int petite_minute = 0;
+	int g_heure = 0;
+	int p_heure = 0;
+	int g_minute = 0;
+	int p_minute = 0;
 
-	for (; grande_heure <= 2; grande_heure++)
+	for (; g_heure <= 2; g_heure++)
 	{
-		for (petite_heure = 0; petite_heure <= 9; petite_heure++)
+		for (p_heure = 0; p_heure <= 9; p_heure++)
 		{
-			for (grande_minute = 0; grande_minute <= 5; grande_minute++)
+			for (g_minute = 0; g_minute <= 5; g_minute++)
 			{
-				for (petite_minute = 0; petite_minute <= 9; petite_minute++)
+				for (p_minute = 0; p_minute <= 9; p_minute++)
 				{
-					_putchar(grande_heure + '0');
-					_putchar(petite_heure + '0');
+					_putchar(g_heure + '0');
+					_putchar(p_heure + '0');
 					_putchar(':');
-					_putchar(grande_minute + '0');
-					_putchar(petite_minute + '0');
+					_putchar(g_minute + '0');
+					_putchar(p_minute + '0');
 					_putchar('\n');
 				}
 			}
-			if (grande_heure == 2 && petite_heure == 3 && grande_minute > 5 && petite_minute > 9)
+			if (g_heure == 2 && p_heure == 3 && g_minute > 5 && p_minute > 9)
 				break;
 		}
 	}
