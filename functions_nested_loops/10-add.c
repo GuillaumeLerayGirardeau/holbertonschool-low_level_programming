6553ca4a7244bdf3c2c0141118269a0cd1796cb4
GuillaumeLerayGirardeau <guiraydeau@gmail.com>
2025-06-20 12:23:29 +0000

    10-add.c function updated, wrong betty coding style

--- functions_nested_loops/10-add.c
+++ functions_nested_loops/10-add.c
@@ -1,7 +1,17 @@
 #include "main.h"
 
+/**
+ * add - add two number
+ * @a : first digit
+ * @b : second digit
+ *
+ * Return: On success 1.
+ * On error, -1 is returned, and errno is set appropriately.
+ */
+
 int add(int a, int b)
 {
 	int result = a + b;
+
 	return (result);
 }
