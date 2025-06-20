06b08a3b298a8bfc5bf3b9e55372227d42ba695a
GuillaumeLerayGirardeau <guiraydeau@gmail.com>
2025-06-20 15:13:48 +0000

    8-24_hours.c function added, main.h updated

---
+++ functions_nested_loops/8-24_hours.c
@@ -0,0 +1,34 @@
+#include "main.h"
+
+/**
+  * jack_bauer - prints every minutes of the day of Jack Bauer
+  *
+  */
+void jack_bauer(void)
+{
+	int grande_heure = 0;
+	int petite_heure = 0;
+	int grande_minute = 0;
+	int petite_minute = 0;
+
+	for (; grande_heure <= 2; grande_heure++)
+	{
+		for (petite_heure = 0; petite_heure <= 9; petite_heure++)
+		{
+			for (grande_minute = 0; grande_minute <= 5; grande_minute++)
+			{
+				for (petite_minute = 0; petite_minute <= 9; petite_minute++)
+				{
+					_putchar(grande_heure + '0');
+					_putchar(petite_heure + '0');
+					_putchar(':');
+					_putchar(grande_minute + '0');
+					_putchar(petite_minute + '0');
+					_putchar('\n');
+				}
+			}
+			if (petite_heure == 3 && grande_minute > 5 && petite_minute > 9)
+				break;
+		}
+	}
+}
