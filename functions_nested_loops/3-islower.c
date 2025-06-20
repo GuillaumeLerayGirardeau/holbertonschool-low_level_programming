65dfa573643d560b33a752288f7eed8bb5027a6f
GuillaumeLerayGirardeau <guiraydeau@gmail.com>
2025-06-20 08:44:46 +0000

    3-islower.c function added, main.h file updated

---
+++ functions_nested_loops/3-islower.c
@@ -0,0 +1,21 @@
+#include "main.h"
+#include <ctype.h>
+
+/**
+  *_islower - check for lowercase character
+ * @c: The character to print
+ *
+ * Return: On success 1.
+ * On error, -1 is returned, and errno is set appropriately.
+ */
+int _islower(int c)
+{
+	if (islower(c))
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
