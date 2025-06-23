4dcc13935ffe3b9f385c947c7363befd5774c208
GuillaumeLerayGirardeau <guiraydeau@gmail.com>
2025-06-23 10:05:38 +0000

    9-fizz_buzz.c program added

---
+++ more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,31 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+  *main - start of the program
+  *Return: 0 is success
+  */
+
+int main(void)
+{
+	int number;
+
+	for (number = 1; number <= 100; number++)
+	{
+		if (number % 3 == 0)
+		{
+			printf("Fizz");
+		}
+		if (number % 5 == 0)
+		{
+			printf("Buzz");
+		}
+		if (number % 5 != 0 && number % 3 != 0)
+		{
+			printf("%d", number);
+		}
+		printf(" ");
+	}
+	printf("\n");
+	return (0);
+}
