a199eb9e0240ba3004bfa38cd8d91f2438264f2c
GuillaumeLerayGirardeau <guiraydeau@gmail.com>
2025-06-25 15:14:40 +0000

    5-rev_string.c function added, main.h updated

---
+++ pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,33 @@
+#include "main.h"
+
+/**
+  * rev_string - reverses a string
+  *
+  * @s : the string
+  *
+  */
+void rev_string(char *s)
+{
+	int number_letter = 0;
+	int debut_letter = 0;
+	int milieu;
+	int swap;
+
+	while (s[number_letter] != '\0')
+	{
+		number_letter++;
+	}
+
+	number_letter -= 1;
+	milieu = number_letter / 2;
+
+	while (debut_letter < milieu)
+	{
+		swap = s[debut_letter];
+		s[debut_letter] = s[number_letter];
+		s[number_letter] = swap;
+		debut_letter++;
+		number_letter--;
+	}
+
+}
