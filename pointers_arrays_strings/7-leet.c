1e542f77f2253f7020b025bc6310b09d7faeea2d
GuillaumeLerayGirardeau <guiraydeau@gmail.com>
2025-06-27 14:24:06 +0000

    7-leet.c function added, main.h updated

--- pointers_arrays_strings/7-leet.c
+++ pointers_arrays_strings/7-leet.c
@@ -12,28 +12,20 @@
 char *leet(char *n)
 {
 	int count = 0;
+	int pos;
+
+	char lettre[] = "AEOTL";
+	int chiffre[] = {'4', '3', '0', '7', '1'};
+
 
 	while (n[count] != '\0')
 	{
-		if (n[count] == 'a' || n[count] == 'A')
+		for (pos = 0; pos <= 4; pos++)
 		{
-			n[count] = '4';
-		}
-		else if (n[count] == 'e' || n[count] == 'E')
-		{
-			n[count] = '3';
-		}
-		else if (n[count] == 'o' || n[count] == 'O')
-		{
-			n[count] = '0';
-		}
-		else if (n[count] == 't' || n[count] == 'T')
-		{
-			n[count] = '7';
-		}
-		else if (n[count] == 'l' || n[count] == 'L')
-		{
-			n[count] = '1';
+			if (n[count] == lettre[pos] || n[count] == (lettre[pos] + 32))
+			{
+				n[count] = chiffre[pos];
+			}
 		}
 		count++;
 	}
