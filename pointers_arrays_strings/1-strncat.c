94a1d4d2191e7f38cd0f994e1ff5249c3d41d002
GuillaumeLerayGirardeau <guiraydeau@gmail.com>
2025-06-27 08:24:03 +0000

    1-strncat.c funtion added, main.h updated

---
+++ pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,32 @@
+#include "main.h"
+
+/**
+  * _strncat - concatenates two strings
+  *
+  * @dest : original string
+  * @src : second string
+  * @n : number of letter from the second string
+  *
+  * Return: merged string
+  */
+char *_strncat(char *dest, char *src, int n)
+{
+	int count = 0;
+	int all_letters = 0;
+
+	n -= 1;
+
+	while (dest[count] != '\0')
+	{
+		count++;
+	}
+
+	all_letters = count;
+
+	for (count = 0; count <= n; count++)
+	{
+		dest[all_letters] = src[count];
+		all_letters++;
+	}
+	return (dest);
+}
