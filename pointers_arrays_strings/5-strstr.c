dc6daeec5b6adbf7603bee2de19ee240b67bf1af
GuillaumeLerayGirardeau <guiraydeau@gmail.com>
2025-07-02 13:50:24 +0000

    5-strstr.c function updated

--- pointers_arrays_strings/5-strstr.c
+++ pointers_arrays_strings/5-strstr.c
@@ -12,36 +12,41 @@
 
 char *_strstr(char *haystack, char *needle)
 {
-	int time_hay = 0;
-	int time_need = 0;
-	int lenght_need = 0;
-	int a = 0;
+	int time_h = 0;
+	int time_n = 0;
 	char *p = NULL;
+	int length = 0;
 
-	while (needle[lenght_need] != '\0')
+	if (haystack[time_h] != '\0' && needle[time_n] != '\0')
 	{
-		lenght_need++;
-	}
-
-	while (haystack[time_hay] != '\0')
-	{
-		a = 0;
-		time_need = 0;
-
-		while (haystack[time_hay] == needle[time_need] && needle[time_need] != '\0')
+		while (needle[time_n] != 0)
 		{
-			time_hay++;
-			time_need++;
-			a++;
+			time_n++;
 		}
 
-		if (a == lenght_need && lenght_need != 0 && needle[0] != '\0')
+		length = time_n;
+
+		while (haystack[time_h] != '\0')
 		{
-			p = (haystack + time_hay) - time_need;
-			break;
+			time_n = 0;
+
+			while (haystack[time_h] == needle[time_n] &&
+					needle[time_n] != '\0' &&
+					haystack[time_h] != '\0')
+			{
+				haystack++;
+				time_n++;
+			}
+
+			if (time_n == length)
+			{
+				p = (haystack + time_h - time_n);
+				return (p);
+			}
+
+			time_h -= time_n;
+			time_h++;
 		}
-		time_hay -= time_need;
-		time_hay++;
 	}
 	return (p);
 }
