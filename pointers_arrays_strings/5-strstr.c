56935f6c21476fa4a64159f5d6d26d5d646a5d9a
GuillaumeLerayGirardeau <guiraydeau@gmail.com>
2025-07-02 14:17:54 +0000

    5-strstr.c function updated

--- pointers_arrays_strings/5-strstr.c
+++ pointers_arrays_strings/5-strstr.c
@@ -14,39 +14,30 @@ char *_strstr(char *haystack, char *needle)
 {
 	int time_h = 0;
 	int time_n = 0;
+	int h = 0;
 	char *p = NULL;
-	int length = 0;
 
-	if (haystack[time_h] != '\0' && needle[time_n] != '\0')
+	if (haystack[time_h] == '\0')
 	{
-		while (needle[time_n] != 0)
+		return (p);
+	}
+
+	while (haystack[time_h] != '\0')
+	{
+		h= time_h;
+
+		for (time_n = 0; haystack[h] == needle[time_n] && haystack[h] != '\0'; h++)
 		{
 			time_n++;
-		}
-
-		length = time_n;
-
-		while (haystack[time_h] != '\0')
-		{
-			time_n = 0;
-
-			while (haystack[time_h] == needle[time_n] &&
-					needle[time_n] != '\0' &&
-					haystack[time_h] != '\0')
+			if (needle[time_n] == '\0')
 			{
-				haystack++;
-				time_n++;
-			}
-
-			if (time_n == length)
-			{
-				p = (haystack + time_h - time_n);
+				p = (haystack + h) - time_n;
 				return (p);
 			}
-
-			time_h -= time_n;
-			time_h++;
 		}
+
+		time_h++;
 	}
+
 	return (p);
 }
