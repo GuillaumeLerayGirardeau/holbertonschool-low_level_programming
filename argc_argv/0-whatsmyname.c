8801b3507dea7e688f403c8da8e211db162627f5
GuillaumeLerayGirardeau <guiraydeau@gmail.com>
2025-07-03 11:42:23 +0000

    0-whatsmyname.c program updated

--- argc_argv/0-whatsmyname.c
+++ argc_argv/0-whatsmyname.c
@@ -1,6 +1,15 @@
 #include "main.h"
 #include <stdio.h>
 
+/**
+  * main - start of the program
+  *
+  * @argc : unused number of attributes
+  * @argv : array of attributes
+  *
+  * Return: 0 if success
+  */
+
 int main(__attribute__((unused)) int argc, char *argv[])
 {
 	printf("%s\n", argv[0]);
