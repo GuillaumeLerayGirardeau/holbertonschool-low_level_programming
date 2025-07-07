0407f8c18369731f780eb5089725b53ea4af5364
GuillaumeLerayGirardeau <guiraydeau@gmail.com>
2025-07-07 16:36:18 +0200

    str_concat fucntion updated

--- malloc_free/2-str_concat.c
+++ malloc_free/2-str_concat.c
@@ -24,7 +24,7 @@ char *str_concat(char *s1, char *s2)
 		for (size2 = 0; s2[size2] != '\0'; size2++)
 		{}
 
-		combo = malloc(sizeof(char) * (size1 + size2));
+		combo = malloc(sizeof(char) * (size1 + size2) + 1);
 
 		if (combo != NULL)
 		{
