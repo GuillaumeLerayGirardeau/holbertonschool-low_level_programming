9d7e88f95c436cc79bc47021fbbce3f6c89e8375
GuillaumeLerayGirardeau <guiraydeau@gmail.com>
2025-07-08 15:49:06 +0200

    Function updated

--- malloc_free/2-str_concat.c
+++ malloc_free/2-str_concat.c
@@ -17,39 +17,46 @@ char *str_concat(char *s1, char *s2)
 	int size1;
 	int size2;
 
-	if (s1 != NULL || s2 != NULL)
+	if (s1 != NULL)
 	{
 		for (size1 = 0; s1[size1] != '\0'; size1++)
 		{}
-		for (size2 = 0; s2[size2] != '\0'; size2++)
-		{}
-
-		combo = malloc(sizeof(char) * (size1 + size2) + 1);
-
-		if (combo != NULL)
-		{
-			for (size1 = 0; s1[size1] != '\0'; size1++)
-			{
-				combo[size1] = s1[size1];
-			}
-			for (size2 = 0; s2[size2] != '\0'; size2++)
-			{
-				combo[size1] = s2[size2];
-				size1++;
-			}
-
-			combo[size1] = '\0';
-
-			return (combo);
-		}
-		else
-		{
-			return (NULL);
-		}
 	}
 	else
 	{
 		return (NULL);
 	}
 
+	if (s2 != NULL)
+	{
+		for (size2 = 0; s2[size2] != '\0'; size2++)
+		{}
+	}
+	else
+	{
+		return (NULL);
+	}
+
+	combo = malloc(sizeof(char) * (size1 + size2 + 1));
+
+	if (combo != NULL)
+	{
+		for (size1 = 0; s1[size1] != '\0'; size1++)
+		{
+			combo[size1] = s1[size1];
+		}
+		for (size2 = 0; s2[size2] != '\0'; size2++)
+		{
+			combo[size1] = s2[size2];
+			size1++;
+		}
+
+		combo[size1] = '\0';
+
+		return (combo);
+	}
+	else
+	{
+		return (NULL);
+	}
 }
