82dc14721337d9d39ce266704cf7899af6455e44
GuillaumeLerayGirardeau <guiraydeau@gmail.com>
2025-07-09 13:55:51 +0200

    Structures_typedef folder added, contains README and header file dog

---
+++ structures_typedef/README.md
@@ -0,0 +1,4 @@
+STRUCTURE AND TYPEDEF
+
+This folder contains several exercises about structures and typedef in C.
+Most of these programs and functions use C programming language.
---
+++ structures_typedef/dog.h
@@ -0,0 +1,18 @@
+#ifndef dog_h
+#define dog_h
+
+/**
+ * struct dog - gives information about a dog
+ * @name : name of the dog
+ * @age : age of the dog
+ * @owner : name of the owner
+ */
+
+struct dog
+{
+char *name;
+float age;
+char *owner;
+};
+
+#endif
