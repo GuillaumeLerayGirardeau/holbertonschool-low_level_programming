b3911a3e2d2e13d01e30b2747297dfe4ee4a6aed
GuillaumeLerayGirardeau <guiraydeau@gmail.com>
2025-07-09 11:18:44 +0200

    Function updated

--- more_malloc_free/1-string_nconcat.c
+++ more_malloc_free/1-string_nconcat.c
@@ -20,6 +20,15 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 	unsigned int i = 0;
 	unsigned int j = 0;
 
+	if (s1 == NULL)
+	{
+		s1 = "";
+	}
+	if (s2 == NULL)
+	{
+		s2 = "";
+	}
+
 	while (s1[size1] != '\0')
 	{
 		size1++;
