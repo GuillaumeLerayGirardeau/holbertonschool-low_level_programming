68a96d13fa49296836b99ae186b393f447a32aae
GuillaumeLerayGirardeau <guiraydeau@gmail.com>
2025-07-09 14:04:15 +0200

    1-init_dog function added, dog.h updated

--- structures_typedef/dog.h
+++ structures_typedef/dog.h
@@ -7,12 +7,13 @@
  * @age : age of the dog
  * @owner : name of the owner
  */
-
 struct dog
 {
 char *name;
 float age;
 char *owner;
 };
+
+void init_dog(struct dog *d, char *name, float age, char *owner);
 
 #endif
