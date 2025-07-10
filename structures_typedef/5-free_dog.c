7771f8412a3fb787944a4a5a3b36df9714151c0a
GuillaumeLerayGirardeau <guiraydeau@gmail.com>
2025-07-10 16:37:39 +0200

    Function updated

--- structures_typedef/5-free_dog.c
+++ structures_typedef/5-free_dog.c
@@ -6,8 +6,12 @@
  * free_dog - Frees memory allocated for a dog_t structure
  * @d: Pointer to the structure to be freed
  */
+
 void free_dog(dog_t *d)
 {
+	if (d == NULL)
+		return;
+
 	free(d->name);
 	free(d->owner);
 	free(d);
