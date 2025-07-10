a917e463075fc0a6b8934b2aea4c44e99859eefb
GuillaumeLerayGirardeau <guiraydeau@gmail.com>
2025-07-10 09:34:33 +0200

    Function updated

--- malloc_free/2-str_concat.c
+++ malloc_free/2-str_concat.c
@@ -19,6 +19,12 @@ char *str_concat(char *s1, char *s2)
 	int i;
 	int j;
 
+	if (s1 == NULL)
+		s1 = "";
+
+	if (s2 == NULL)
+		s2 = "";
+
 	for (size1 = 0; s1[size1] != '\0'; size1++)
 	{}
 	for (size2 = 0; s2[size2] != '\0'; size2++)
