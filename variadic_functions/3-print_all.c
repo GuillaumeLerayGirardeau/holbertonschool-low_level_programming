c65d4e0d41cedc40fd290bd48710511cb30be57c
GuillaumeLerayGirardeau <guiraydeau@gmail.com>
2025-07-17 13:36:20 +0200

    Function updated

--- variadic_functions/3-print_all.c
+++ variadic_functions/3-print_all.c
@@ -9,33 +9,33 @@
 
 void print_all(const char * const format, ...)
 {
-	int sep = 0, count = 0, correct = 0;
+	int sep = 0, count = 0;
 	char *str;
 	va_list print;
 
 	va_start(print, format);
 	while (format[count] != '\0')
 	{
-		if (sep == 1 && format[count + 1] != '\0' && correct == 1)
+		if (sep == 1 && (format[count] == 'c' || format[count] == 'i'
+			|| format[count] == 'f' || format[count] == 's'))
 			printf(", ");
-		correct = 0;
 		switch (format[count])
 		{
 			case 'c':
 				printf("%c", va_arg(print, int));
-				sep = 1, correct = 1;
+				sep = 1;
 				break;
 			case 'i':
 				printf("%d", va_arg(print, int));
-				sep = 1, correct = 1;
+				sep = 1;
 				break;
 			case 'f':
 				printf("%f", va_arg(print, double));
-				sep = 1, correct = 1;
+				sep = 1;
 				break;
 			case 's':
 				str = va_arg(print, char*);
-				sep = 1, correct = 1;
+				sep = 1;
 				if (str == NULL)
 				{printf("(nil)");
 					break; }
