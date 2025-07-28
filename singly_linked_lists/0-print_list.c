37a54222275652519dbe5bfcdbe00e14fca4b314
GuillaumeLerayGirardeau <guiraydeau@gmail.com>
2025-07-28 11:11:41 +0200

    func: function print_list added and header file lists.h

---
+++ singly_linked_lists/0-print_list.c
@@ -0,0 +1,40 @@
+#include "lists.h"
+
+/**
+ * check_null - check if the list points to null value
+ *
+ * @h: list to check
+ *
+ * Return: 1 if success
+ */
+unsigned int check_null(const list_t *h)
+{
+	if (h->next != NULL)
+	{
+		print_list(h->next);
+	}
+	return (1);
+}
+
+/**
+ * print_list - prints all the elements of a list_t list
+ *
+ * @h: list to print
+ *
+ * Return: number of nodes
+ */
+size_t print_list(const list_t *h)
+{
+	if (h->str == NULL)
+	{
+		printf("[0] (nil)\n");
+		return (1 + check_null(h));
+	}
+	else
+	{
+		printf("[%d] %s\n", h->len, h->str);
+		return (1 + check_null(h));
+	}
+
+	return (0);
+}
