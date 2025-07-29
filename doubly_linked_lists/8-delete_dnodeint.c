e1c200bf56be199b3101bf50834c9fcf29821cc6
GuillaumeLerayGirardeau <guiraydeau@gmail.com>
2025-07-29 17:08:40 +0200

    fix: betty style and description added

--- doubly_linked_lists/8-delete_dnodeint.c
+++ doubly_linked_lists/8-delete_dnodeint.c
@@ -23,27 +23,30 @@ unsigned int total_node(dlistint_t **h)
 	return (total - 1);
 }
 
+/**
+ * delete_dnodeint_at_index - deletes the node at index of a linked list
+ *
+ * @head: first node of the list
+ * @index: index of the node that should be deleted
+ *
+ * Return: 1 if success, -1 if fail
+ */
 int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
 {
 	dlistint_t *position, *temp;
 	unsigned int count = 0, total = 0;
 
 	if (*head == NULL)
-	{
 		return (-1);
-	}
-
 	position = *head;
 	total = total_node(head);
 	if (index > total)
 		return (-1);
-
 	while (count < index)
 	{
 		position = position->next;
 		count++;
 	}
-
 	if (index == 0)
 	{
 		position = *head;
