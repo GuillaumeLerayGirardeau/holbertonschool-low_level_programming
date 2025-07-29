78dd5d289da95131bbc576ad7edbeb2baea9038b
GuillaumeLerayGirardeau <guiraydeau@gmail.com>
2025-07-29 16:26:28 +0200

    fix: corrected output

--- doubly_linked_lists/7-insert_dnodeint.c
+++ doubly_linked_lists/7-insert_dnodeint.c
@@ -37,7 +37,7 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 	dlistint_t *newnode, *address, *prev_address;
 	unsigned int position = 0, total = 0;
 
-	if (*h == NULL)
+	if (*h == NULL && idx != 0)
 		return (NULL);
 
 	address = *h;
