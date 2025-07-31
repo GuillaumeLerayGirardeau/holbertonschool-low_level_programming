d6c1bd0cf1f7e191e71dd8565f2c714b3b294fe5
GuillaumeLerayGirardeau <guiraydeau@gmail.com>
2025-07-31 15:24:30 +0200

    fix: add error if write fails

--- file_io/0-read_textfile.c
+++ file_io/0-read_textfile.c
@@ -33,6 +33,8 @@ ssize_t read_textfile(const char *filename, size_t letters)
 		letters = size;
 
 	write(1, str, letters);
+	if ((write(1, str, letters)) == -1)
+		return (0);
 
 	free(str);
 	close(fd);
