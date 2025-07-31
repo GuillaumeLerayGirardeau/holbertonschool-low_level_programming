4686e690605dcbdaf75696e85cdc7d56969a1875
GuillaumeLerayGirardeau <guiraydeau@gmail.com>
2025-07-31 16:28:08 +0200

    fix: wrong error ouput

--- file_io/2-append_text_to_file.c
+++ file_io/2-append_text_to_file.c
@@ -46,7 +46,7 @@ int append_text_to_file(const char *filename, char *text_content)
 	size = _strlen(text_content);
 
 	write_check = write(fd, text_content, size);
-	if (write_check == -1)
+	if (write_check == -1 || write_check < size)
 	{
 		close(fd);
 		return (-1);
