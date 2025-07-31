03160ea834cf5903d2269914304a2c5a0d1c56c4
GuillaumeLerayGirardeau <guiraydeau@gmail.com>
2025-07-31 16:24:33 +0200

    fix: close file when error occurs

--- file_io/2-append_text_to_file.c
+++ file_io/2-append_text_to_file.c
@@ -47,6 +47,7 @@ int append_text_to_file(const char *filename, char *text_content)
 
 	write_check = write(fd, text_content, size);
 	if (write_check == -1)
+		close(fd);
 		return (-1);
 
 	close(fd);
