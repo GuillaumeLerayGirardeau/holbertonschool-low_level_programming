3ea98e5686b2d60e91405f4d0cfa508091cc7ca9
GuillaumeLerayGirardeau <guiraydeau@gmail.com>
2025-08-01 16:33:28 +0200

    fix: type changes for check and true_number variables

--- file_io/3-cp.c
+++ file_io/3-cp.c
@@ -12,9 +12,9 @@
  */
 int main(int argc, char *argv[])
 {
-	int fd;
-	int check;
-	int true_number = 0;
+	int fd_from, fd_to;
+	ssize_t check;
+	ssize_t true_number = 0;
 	char *file_copy[1024];
 	mode_t old_umask;
 
@@ -24,47 +24,47 @@ int main(int argc, char *argv[])
 		exit(97);
 	}
 
-	fd = open(argv[1], O_RDONLY);
-	if (fd == -1)
+	fd_from = open(argv[1], O_RDONLY);
+	if (fd_from == -1)
 	{
 		dprintf(2, "Can't read from file %s\n", argv[1]);
 		exit(98);
 	}
 
-	true_number = read(fd, file_copy, 1024);
+	true_number = read(fd_from, file_copy, 1024);
 	if (true_number == -1)
 	{
 		dprintf(2, "Can't read from file %s\n", argv[1]);
 		exit(98);
 	}
 
-	check = close(fd);
+	check = close(fd_from);
 	if (check == -1)
 	{
-		dprintf(2, "Can't close fd %i\n", fd);
+		dprintf(2, "Can't close fd %i\n", fd_from);
 		exit(100);
 	}
 
 	old_umask = umask(0);
-	fd = open(argv[2], O_WRONLY | O_TRUNC | O_CREAT, 0664);
+	fd_to = open(argv[2], O_WRONLY | O_TRUNC | O_CREAT, 0664);
 	umask(old_umask);
-	if (fd == -1)
+	if (fd_to == -1)
 	{
 		dprintf(2, "Can't write to %s\n", argv[2]);
 		exit(99);
 	}
 
-	check = write(fd, file_copy, true_number);
+	check = write(fd_to, file_copy, true_number);
 	if (check == -1 || check != true_number)
 	{
 		dprintf(2, "Can't write to %s\n", argv[2]);
 		exit(99);
 	}
 
-	check = close(fd);
+	check = close(fd_to);
 	if (check == -1)
 	{
-		dprintf(2, "Can't close fd %i\n", fd);
+		dprintf(2, "Can't close fd %i\n", fd_to);
 		exit(100);
 	}
 	return (0);
